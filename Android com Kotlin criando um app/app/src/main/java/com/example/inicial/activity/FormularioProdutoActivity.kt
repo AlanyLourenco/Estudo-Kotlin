35b023164443b09141ac92ca444a68476a42b9e4
Alany Lourenço <95310758+AlanyLourenco@users.noreply.github.com>
2023-08-22 16:29:04 -0300

    Inicial

---
+++ Android com Kotlin criando um app/app/src/main/java/com/example/inicial/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.inicial.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.inicial.R
+
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+
+}
